a13956b1d82abdcb2ba74d873aec5a4e2f52f0a1
Ada-Kiekhaefer <ada.kiekhaefer@gmail.com>
2020-02-22 10:18:34 -0600

    histogram plot on Shiny

--- credit_risk_log_model_Shiny.R
+++ credit_risk_log_model_Shiny.R
@@ -4,36 +4,49 @@ library(ggplot2)
 library(dplyr)
 library(readr)
 
-load('./data/data_and_log_model.RData')
+load('./data/loan_data_no_outliers.RData')
+#log_model<- glm(loan_status ~ age + annual_inc, family='binomial', data = loan_data_no_outliers)
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      textInput('stock_symbol', 'Enter stock symbol: ', 'AAPL')),
+  textInput('borrower_age', 'Enter age: '),
+  textInput('borrower_annual_income', 'Enter annual income: '),
+  selectInput("indepvar", label = h3("Explanatory variable: "), 
+              choices = list("annual income" = "annual_inc",
+                             "age" = "age"), selected = 1)),
     mainPanel(
-      textOutput("symbol"),
-      plotOutput('trend'),
-      plotOutput('volume'))
+  textOutput('age'),
+  textOutput('annual_income'),
+  plotOutput('distribution1')),
+
   )
 )
 server <- function(input, output){
-  output$symbol <- renderText({
-    paste(toupper(input$stock_symbol), 'stock time-series plot')
+  output$age <- renderText({
+    paste('Age : ', input$borrower_age, '\n')
   })
-  output$trend <- renderPlot({
-    # ggplot(subset(all_stocks, Name == toupper(input$stock_symbol))) +
-    #   geom_line(aes(x = date, y = close)) +
-    #   labs(y = 'stock price (dollars)')
-    s_name <- all_stocks %>% 
-      filter(Name == toupper(input$stock_symbol))
-    ggplot(s_name) +
-      geom_line(aes(x = date, y = close)) +
-      labs(y = 'stock price (dollars)')
-  }) 
-  output$volume <- renderPlot({
-    ggplot(subset(all_stocks, Name == toupper(input$stock_symbol))) +
-      geom_line(aes(x = date, y = volume)) +
-      labs(y = 'stock volume')
+  output$annual_income <- renderText({
+    paste('Annual income : ', input$borrower_annual_income)
   })
+
+  # # Regression output
+  # output$summary <- renderPrint({
+  #   fit <-lm(loan_data_no_outliers[,loan_status] ~ loan_data_no_outliers[,annual_inc])
+  #   #names(fit$coefficients) <- c("Intercept", annual_inc)
+  #   summary(fit)
+  # })
+  
+  # Histogram output var 1
+  output$distribution1 <- renderPlot({
+    hist(loan_data_no_outliers[,input$indepvar], main="", xlab=input$indepvar)
+  }, height=300, width=300)
+  
+  # output$model_result <- renderText({
+  #   # newdata <- as.data.frame(age=input$borrower_age, annual_inc = input$borrower_annual_income)
+  #   # p <- predict(log_model, newdata = newdata, type = 'response')
+  #   # paste('model prob = ', as.character(p))
+  # })
+
 }
 shinyApp(ui = ui, server = server)
