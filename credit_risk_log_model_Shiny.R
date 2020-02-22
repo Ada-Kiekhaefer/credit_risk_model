773437b8fd3c5cce1c3720534f17dafdcddffa69
Ada-Kiekhaefer <ada.kiekhaefer@gmail.com>
2020-02-22 10:39:25 -0600

    independent variables plot on Shiny

--- credit_risk_log_model_Shiny.R
+++ credit_risk_log_model_Shiny.R
@@ -12,14 +12,21 @@ ui <- fluidPage(
     sidebarPanel(
   textInput('borrower_age', 'Enter age: '),
   textInput('borrower_annual_income', 'Enter annual income: '),
-  selectInput("indepvar", label = h3("Explanatory variable: "), 
+  selectInput("indepvar", label = "Explanatory variables histrogram: ", 
               choices = list("annual income" = "annual_inc",
-                             "age" = "age"), selected = 1)),
+                             "loan amount" = "loan_amnt",
+                             "age" = "age"), selected = 1),
+  selectInput("indepvar2", label = "Explanatory variables plot: ", 
+              choices = list("grade" = "grade",
+                             "home ownership" = "home_ownership",
+                             "interest rate" = "ir_cat",
+                             "employment length" = "emp_cat"), selected = 1)),  
     mainPanel(
   textOutput('age'),
   textOutput('annual_income'),
-  plotOutput('distribution1')),
-
+  plotOutput('distribution1'),
+  plotOutput('distribution2')),
+  
   )
 )
 server <- function(input, output){
@@ -39,9 +46,16 @@ server <- function(input, output){
   
   # Histogram output var 1
   output$distribution1 <- renderPlot({
-    hist(loan_data_no_outliers[,input$indepvar], main="", xlab=input$indepvar)
+    hist(loan_data_no_outliers[,input$indepvar], 
+         main="Histogram of independent variable", xlab=input$indepvar)
   }, height=300, width=300)
-  
+
+  #Plot of other independent variables
+  output$distribution2 <- renderPlot({
+    plot(loan_data_no_outliers[,input$indepvar2], 
+         main="Plot of independent variable", xlab=input$indepvar2)
+  }, height=300, width=300)
+    
   # output$model_result <- renderText({
   #   # newdata <- as.data.frame(age=input$borrower_age, annual_inc = input$borrower_annual_income)
   #   # p <- predict(log_model, newdata = newdata, type = 'response')
