0bb3b90e08efe693220fccb82ff568a9ae9cb62b
Ada-Kiekhaefer <ada.kiekhaefer@gmail.com>
2020-02-24 14:42:38 -0600

    show model summary Shiny

--- credit_risk_log_model_Shiny.R
+++ credit_risk_log_model_Shiny.R
@@ -25,7 +25,8 @@ ui <- fluidPage(
   textOutput('age'),
   textOutput('annual_income'),
   plotOutput('distribution1'),
-  plotOutput('distribution2')),
+  plotOutput('distribution2'),
+  verbatimTextOutput("summary")),
   
   )
 )
@@ -37,30 +38,57 @@ server <- function(input, output){
     paste('Annual income : ', input$borrower_annual_income)
   })
 
-  # # Regression output
-  # output$summary <- renderPrint({
-  #   fit <-lm(loan_data_no_outliers[,loan_status] ~ loan_data_no_outliers[,annual_inc])
-  #   #names(fit$coefficients) <- c("Intercept", annual_inc)
-  #   summary(fit)
-  # })
   
   # Histogram output var 1
   output$distribution1 <- renderPlot({
     hist(loan_data_no_outliers[,input$indepvar], 
          main="Histogram of independent variable", xlab=input$indepvar)
-  }, height=300, width=300)
+  }, height=400, width=400)
 
   #Plot of other independent variables
   output$distribution2 <- renderPlot({
     plot(loan_data_no_outliers[,input$indepvar2], 
          main="Plot of independent variable", xlab=input$indepvar2)
-  }, height=300, width=300)
-    
-  # output$model_result <- renderText({
-  #   # newdata <- as.data.frame(age=input$borrower_age, annual_inc = input$borrower_annual_income)
-  #   # p <- predict(log_model, newdata = newdata, type = 'response')
-  #   # paste('model prob = ', as.character(p))
-  # })
+  }, height=400, width=400)
+
+  output$summary <- renderPrint({
+    fit <- glm(loan_status ~ age + annual_inc, family = 'binomial', data = loan_data_no_outliers)
+    summary(fit)
+  })
+  
+  output$prediction <- renderText({
+    # newdata <- as.data.frame(age=input$borrower_age, annual_inc = input$borrower_annual_income)
+    # p <- predict(log_model, newdata = newdata, type = 'response')
+    # paste('model prob = ', as.character(p))
+  })
 
 }
 shinyApp(ui = ui, server = server)
+
+# ### Test in regular R command
+# newdata <- data.frame('age'=33, 'annual_inc' = 100000)
+# p <- predict(log_model, newdata = newdata, type = 'response')
+# paste('model prob = ', as.character(p))
+# # Make a binary predictions-vector using a cut-off of 15%
+# p_cutoff_15 <- ifelse(p > 0.15, 1, 0)
+# p_cutoff_15 <- ifelse(p > 0.15, 'Default', 'Non default')
+# approval <- ifelse(p > 0.15, 'Not approved', 'Approved')
+# print(p_cutoff_15)
+# print(approval)
+# 
+# log_model<- glm(loan_status ~ age + annual_inc + loan_amnt + grade + home_ownership + emp_cat, 
+#                 family='binomial', data = loan_data_no_outliers)
+# newdata <- data.frame('age'=33, 
+#                       'annual_inc' = 100000,
+#                       'loan_amnt' = 200000,
+#                       'grade' = 'G',
+#                       'home_ownership' = 'RENT',
+#                       'emp_cat' = '0-15')
+# p <- predict(log_model, newdata = newdata, type = 'response')
+# # Make a binary predictions-vector using a cut-off of 15%
+# p_cutoff_15 <- ifelse(p > 0.15, 1, 0)
+# p_cutoff_15 <- ifelse(p > 0.15, 'Default', 'Non default')
+# approval <- ifelse(p > 0.15, 'Not approved', 'Approved')
+# print(p_cutoff_15)
+# print(approval)
+
